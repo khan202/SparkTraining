f59537708f9dd9241dce1a8a2edf252ca4198954
ImranKhan <39298439+khan202@users.noreply.github.com>
2019-10-24 15:30:22 +0530

    updated

--- src/main/scala/SparkSQL/displayTopSecondRecord.scala
+++ src/main/scala/SparkSQL/displayTopSecondRecord.scala
@@ -30,7 +30,7 @@ object displayTopSecondRecord {
     ----------------------------
     */
 
-    val products = spark.read.option("inferschema", true).option("header", true).option("delimiter", ",").format("csv").load("G:\\Spark\\data-master\\SampleData\\products.csv")
+    //val products = spark.read.option("inferschema", true).option("header", true).option("delimiter", ",").format("csv").load("G:\\Spark\\data-master\\SampleData\\products.csv")
     // products.show()
     val customers = spark.read.option("header", true).option("inferschema", true).option("delimiter", ",").option("encoding", "ISO-8859-1").csv("G:\\Spark\\data-master\\SampleData\\customers.csv")
     customers.show(1000)
@@ -38,19 +38,30 @@ object displayTopSecondRecord {
     import spark.implicits._
 
     //val Customers = spark.read.option("inferschema", true).option("headers", true).load("G:\\Spark\\data-master\\SampleData\\customers.txt")
+
     val orders = spark.read.option("inferschema", true).option("header", true).format("csv").load("G:\\Spark\\data-master\\SampleData\\orders.csv")
+    val orderdetail = spark.read.option("inferschema", true).option("header", true).format("csv").load("G:\\Spark\\data-master\\SampleData\\orderdetails.csv")
+    val products = spark.read.option("inferschema", true).option("header", true).format("csv").option("encoding", "ISO-8859-1").load("G:\\Spark\\data-master\\SampleData\\products.csv")
+
     val customerJoinOrders = customers.join(orders, "CustomerID")
-    customerJoinOrders.show(5)
+    //customerJoinOrders.show(5)
     val res = customerJoinOrders.select($"*").where("OrderDate LIKE '%1997'").groupBy("CustomerID", "CustomerName").agg(count("OrderID").alias("totalOrders")).orderBy(col("totalOrders").desc)
-    res.show()
+    //res.show()
 
     import org.apache.spark.sql.expressions._
     val rnk = Window.orderBy(col("totalOrders").desc)
 
     val res2 = dense_rank().over(rnk)
     res.select($"CustomerID", $"CustomerName", $"totalOrders", res2.alias("customerRank")).where("customerRank==2").show()
-
-
+    val ordertotals = customerJoinOrders.join(orderdetail, "OrderID")
+    val orderproducts = ordertotals.join(products, "ProductID").orderBy($"orderID".desc)
+    orderproducts.show()
+    val totalAmountPerOrder = orderproducts.select($"CustomerID", $"OrderID", (col("Quantity") * col("Price")).alias("totalorderAmount")).groupBy("CustomerID", "OrderID").agg(sum("totalorderAmount").alias("totalAmountperorder")).orderBy(col("totalAmountperorder").desc)
+    totalAmountPerOrder.show()
+    //totalAmountPerOrder.select($"CustomerID", $"OrderID", $"totalorderAmount").groupBy("CustomerID","orderID").agg(sum($"totalorderAmount")).orderBy($"totalorderAmount".desc).show()
+    //totalAmountPerOrder.show()
+    //totalAmountPerOrder.select($"*").groupBy("orderID").agg(sum("totalorderAmount")).orderBy($"totalorderAmount".desc)
+    //val orderProducts = orderdetail.join(products,"ProductID")
     //customerJoinOrders.select("CustomerID","CustomerName","Country","OrderID","OrderDate").groupBy("CustomerID").agg(count("OrderID").alias("totalOrders")).orderBy(desc("totalOrders")).show()
     //val Refund = spark.read.option("inferschema", true).option("headers", true).load("G:\\Spark\\data-master\\SampleData\\products.txt")
     //products.show()
