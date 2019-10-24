9232d97185ada08fb0651753294f1ecdb4744cc3
ImranKhan <39298439+khan202@users.noreply.github.com>
2019-10-24 15:36:05 +0530

    Update removeSpaces.scala

--- src/main/scala/SparkSQL/removeSpaces.scala
+++ src/main/scala/SparkSQL/removeSpaces.scala
@@ -13,12 +13,36 @@ object removeSpaces {
     val sc = spark.sparkContext
     sc.setLogLevel("ERROR")
     import spark.implicits._
-
+//*******************Remove spaces in between the rows data***********************************//
     val df = Seq(("  p a   b l o", "Paraguay"),
       ("Neymar", "B r    asil")
     ).toDF("name", "country")
-
+    import org.apache.spark.sql.functions._
     df.show()
+    val actualdf = Seq("name", "country").foldLeft(df) { (memoDF, colName) =>
+      memoDF.withColumn(colName, regexp_replace(col(colName),"\\s+",""))
+
+    }
+    actualdf.show()
+//***********************remove spaces in the between the column names and change to lowercase*********************//
+    val sourceDF = Seq(
+      ("funny", "joke")
+    ).toDF("A b C", "de F")
+
+    sourceDF.show()
+    val actualDF = sourceDF
+      .columns
+      .foldLeft(sourceDF) { (memoDF, colName) =>
+        memoDF
+          .withColumnRenamed(
+            colName,
+            colName.toLowerCase().replace(" ", "_")
+          )
+      }
+
+    actualDF.show()
+
+
 
   }
 
