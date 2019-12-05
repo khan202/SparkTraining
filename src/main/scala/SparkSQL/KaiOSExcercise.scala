421955df86b45aea76b2d505186dc5f53e0a0822
ImranKhan <39298439+khan202@users.noreply.github.com>
2019-12-05 23:17:20 +0530

    Add files via upload

--- src/main/scala/SparkSQL/KaiOSExcercise.scala
+++ src/main/scala/SparkSQL/KaiOSExcercise.scala
@@ -1,6 +1,6 @@
 package SparkSQL
+
 import com.typesafe.config.ConfigFactory
-import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
@@ -12,28 +12,26 @@ object KaiOSExcercise {
     val conf = ConfigFactory.load("application.conf")
     val spark = SparkSession.builder().appName(conf.getString("appName"))
       .config("spark.executor.memory", conf.getString("executorMemory"))
-      .config("master", conf.getString("master"))
+      .config("spark.master", conf.getString("master"))
       .config("spark.driver.memory", conf.getString("driverMemory"))
       .getOrCreate()
 
     val data = spark.read.format("com.databricks.spark.csv")
       .option("delimiter", "\t")
       .option("header", false)
-      .option("inferSchema", true).load("G:\\Spark\\lastfm-dataset-1K\\userid-timestamp-artid-artname-traid-traname.tsv")
-      .toDF("userid", "timestamp", "musicbrainz-artist-id", "artist-name", "musicbrainz-track-id", "track-name")
+      .option("inferSchema", true).load(conf.getString("DATA_DIR")+conf.getString("transactions"))//"G:\\Spark\\lastfm-dataset-1K\\userid-timestamp-artid-artname-traid-traname.tsv")
+      .toDF("userid", "timestamp", "artistid", "artistname", "trackid", "trackname")
 
-    //data.show()
+
     import spark.implicits._
-    val distinctUsers = data.select("userid", "musicbrainz-track-id").distinct()
+    val distinctUsers = data.select("userid", "trackid").distinct()
+      .groupBy("userid").agg(count("userid").alias("distincttracks")).orderBy($"distincttracks".desc)
 
-
-
-    //distinctUsers.show()
-
-   distinctUsers.select($"userid", count($"userid").alias("distinctsongs").desc).show()
-
-//distinctUserIds.select($"userid", count($"userid").alias("distinctsongs")).groupBy("userid")
-
+    // distinctUsers.write.format("csv").option("delimeter","\t").save(conf.getString("DATA_DIR")+"solution-1-result")
+    distinctUsers.coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .option("header", "true")
+      .save(conf.getString("DATA_DIR") + "solution-1-result")
 
   }
 
